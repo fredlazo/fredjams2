3d6b2275aa7b985089815df87640bf2baca38ab7
Frederick Lazo <fredlazo@yahoo.com>
2017-01-04 12:23:42 -0800

    Checkpoint 22 and Assignment 22 completed

---
+++ scripts/landing.js
@@ -0,0 +1,14 @@
+            var animatePoints = function(){
+                var points = document.getElementsByClassName('point');
+                
+                var revealPoint = function(i){
+                    points[i].style.opacity = 1;
+                    points[i].style.transform = "scaleX(1) translateY(0)";
+                    points[i].style.msTransform = "scaleX(1) translateY(0)";
+                    points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+                };        
+                
+                for (var i = 0; i <=2; i++){
+                    revealPoint(i);
+                }
+            };
